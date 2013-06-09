b63b42b6d6596ae8642cb33717e365dec3685554
Vyacheslav O. Koshcheyev <vkoscheev@mail.ru>
2013-06-09 16:51:17 +0700

    scoreboard implemented

--- src/game/objects/Scoreboard.cpp
+++ src/game/objects/Scoreboard.cpp
@@ -1,2 +1,30 @@
 
 #include "Scoreboard.h"
+
+
+
+
+CScoreboard::CScoreboard(CGameObject *pParent) : 
+	CGameObject(pParent),
+	m_iScoreVal(0)
+{
+}
+
+
+CScoreboard::~CScoreboard()
+{
+}
+
+
+score_t CScoreboard::GetScore(void)
+{
+	return m_iScoreVal;
+}
+
+
+void CScoreboard::AddScore(score_t iScore)
+{
+	m_iScoreVal += iScore;
+}
+
+
--- src/game/objects/Scoreboard.h
+++ src/game/objects/Scoreboard.h
@@ -5,7 +5,19 @@
 #include "GameObject.h"
 
 
+typedef unsigned long score_t;
+
+
 class CScoreboard : public CGameObject
 {
+public:
+	CScoreboard(CGameObject *pParent);
+	virtual ~CScoreboard();
 
+public:
+	score_t GetScore(void);
+	void AddScore(score_t iScore);
+
+private:
+	score_t m_iScoreVal;
 };
