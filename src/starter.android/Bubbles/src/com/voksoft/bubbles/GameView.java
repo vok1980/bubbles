fa8b5466a7780efbc25a35034a8e80fa4fdca1d4
vok1980 <vok1980@gmail.com>
2013-06-13 02:25:22 +0700

    On touch handling implemented

--- src/starter.android/Bubbles/src/com/voksoft/bubbles/GameView.java
+++ src/starter.android/Bubbles/src/com/voksoft/bubbles/GameView.java
@@ -1,27 +1,73 @@
 package com.voksoft.bubbles;
 
+import java.util.Queue;
+import java.util.concurrent.ConcurrentLinkedQueue;
+
 import javax.microedition.khronos.egl.EGLConfig;
 import javax.microedition.khronos.opengles.GL10;
 
 import android.content.Context;
 import android.graphics.PixelFormat;
 import android.opengl.GLSurfaceView;
+import android.view.MotionEvent;
 
 
 
 public class GameView extends GLSurfaceView 
 {
+	class Point
+	{
+		Point(long _x, long _y)
+		{
+			x = _x;
+			y = _y;
+		}
+		
+		long x;
+		long y;
+	};
+	
+	private Queue<Point> mTouchPoints = new ConcurrentLinkedQueue<Point>();
+	
 	public GameView(Context context) 
 	{
 		super(context);
-		setRenderer(new Renderer());
+		setRenderer(new Renderer(mTouchPoints));
+	}
+	
+	@Override
+	public boolean onTouchEvent(MotionEvent event) 
+	{
+		if (MotionEvent.ACTION_DOWN == event.getAction())
+		{
+//			NativeCaller.OnClick((long)event.getX(), (long)event.getY());
+			mTouchPoints.offer(new Point((long)event.getX(), (long)event.getY()));
+			return true;
+		}
+		
+		// TODO Auto-generated method stub
+		return super.onTouchEvent(event);
 	}
 	
 	
 	private static class Renderer implements GLSurfaceView.Renderer 
 	{
+		final Queue<Point> mTouchPoints;
+		
+		Renderer( Queue<Point> touchPoints )
+		{
+			mTouchPoints = touchPoints;			
+		}
+		
         public void onDrawFrame(GL10 gl) 
         {
+        	Point point = null;
+        	
+        	while ( null != (point = mTouchPoints.poll()) )
+        	{        		
+        		NativeCaller.OnClick(point.x, point.y);
+        	}
+        	 
         	NativeCaller.Update();
         }
 
