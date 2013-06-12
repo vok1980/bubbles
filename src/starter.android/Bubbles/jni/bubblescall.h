49997dcef6429f2d034303f087a3a9458709de16
Vyacheslav O. Koshcheyev <vkoscheev@mail.ru>
2013-06-12 13:53:06 +0700

    creating jni calls

--- src/starter.android/Bubbles/jni/bubblescall.h
+++ src/starter.android/Bubbles/jni/bubblescall.h
@@ -1,1 +1,19 @@
- 
+
+#pragma once
+
+#ifdef __cplusplus
+extern "C" {
+#endif
+
+
+JNIEXPORT void JNICALL Java_com_voksoft_bubbles_NativeCaller_nativeInit(JNIEnv * env, jobject jobj, jint iWidth, jint iHeight);
+JNIEXPORT void JNICALL Java_com_voksoft_bubbles_NativeCaller_nativeUpdate(JNIEnv * env, jobject jobj);
+
+
+#ifdef __cplusplus
+}
+#endif
+#endif
+
+
+
